1cbc798ce0abc18e1496b39a406e18df7e90aea6
Minahil - Fatima <fatimaminahal26@gmail.com>
2025-08-24 21:45:07 +0300

    Commit5

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false,
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
+        manualChunks: undefined,
+      },
+    },
+  },
 })
